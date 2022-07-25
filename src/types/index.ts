06dfb815fa721b12b01a88a2ce1b6c5d326936f1
spietrek <spietrek@gmail.com>
2022-07-25 12:23:26 -0400

    move types to separate files

--- src/types/index.ts
+++ src/types/index.ts
@@ -14,29 +14,3 @@ const AllSeverities = {
 } as const
 
 export type Severities = typeof AllSeverities[keyof typeof AllSeverities]
-
-export type IconStatusType = 'inReview' | 'approved' | 'warning'
-
-export type ButtonColors =
-  | 'inherit'
-  | 'primary'
-  | 'secondary'
-  | 'success'
-  | 'error'
-  | 'info'
-  | 'warning'
-
-export type ButtonSizes = 'small' | 'medium' | 'large'
-
-export type ButtonVariants = 'text' | 'outlined' | 'contained'
-
-export type ButtonAriaPopus =
-  | boolean
-  | 'dialog'
-  | 'menu'
-  | 'true'
-  | 'false'
-  | 'grid'
-  | 'listbox'
-  | 'tree'
-  | undefined
