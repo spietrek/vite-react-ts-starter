a56f74ad8546a95b0b9a6913afd982d1d2e3112b
spietrek <spietrek@gmail.com>
2022-11-11 15:23:29 -0500

    add alerts mock data

---
+++ src/stories/data/alerts.ts
@@ -0,0 +1,28 @@
+import type { IAlert } from '../../types/common/alert.type'
+
+export const alertsData: IAlert[] = [
+  {
+    id: 1,
+    label: 'Alert number 1 text',
+    variant: 'info',
+    visible: true,
+  },
+  {
+    id: 2,
+    label: 'Alert number 2 text',
+    variant: 'success',
+    visible: true,
+  },
+  {
+    id: 3,
+    label: 'Alert number 3 text',
+    variant: 'warning',
+    visible: true,
+  },
+  {
+    id: 4,
+    label: 'Alert number 4 text',
+    variant: 'error',
+    visible: true,
+  },
+]
